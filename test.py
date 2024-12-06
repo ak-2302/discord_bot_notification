48ef0a49a776cc628b77cb7763d13ba0cd27ccaa
ak-2302 <s2211879@s.tsukuba.ac.jp>
2024-12-06 23:41:32 +0900

    5

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ def check_python_version():
     major = sys.version_info.major
     minor = sys.version_info.minor
     micro = sys.version_info.micro
-
     print(f"Python バージョン: {version}")
     print(f"バージョン情報: {major}.{minor}.{micro}")
 
