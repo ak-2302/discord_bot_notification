9db4dd3fed425c6a2b86863e9fac7058529feed4
ak-2302 <s2211879@s.tsukuba.ac.jp>
2024-12-06 23:21:04 +0900

    4

---
+++ test.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def check_python_version():
+    version = sys.version
+    major = sys.version_info.major
+    minor = sys.version_info.minor
+    micro = sys.version_info.micro
+
+    print(f"Python バージョン: {version}")
+    print(f"バージョン情報: {major}.{minor}.{micro}")
+
+
+check_python_version()
